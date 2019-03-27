413fc1eb5870f16f60a36fd9c590ce4ae66f063c
David Recuenco <david.recuenco@adidas-group.com>
2019-03-27 15:39:08 +0100

    UPDATE typescript tooling

--- examples/typescript-browser/webpack.config.js
+++ examples/typescript-browser/webpack.config.js
@@ -1,7 +1,8 @@
-const { output, devtool } = require('build-tools-webpack');
+const { mode, output, devtool } = require('build-tools-webpack');
 const { rules, extensions } = require('build-tools-webpack-typescript');
 
 module.exports = {
+  mode,
   entry: './src/index.ts',
   output,
   module: {
--- packages/typescript/CHANGELOG.md
+++ packages/typescript/CHANGELOG.md
@@ -1,3 +1,8 @@
+# 1.1.0
+
+- Updated typescript to v3.3.
+- Updated typedoc and tslint.
+
 # 1.0.1
 
 - Initial version: `build-tools-typescript`.
